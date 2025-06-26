3100d5da218efc451afadd18b5094c1cad0ea3f5
MokraneMassi <Mokranemassi06@gmail.com>
2025-06-26 11:43:55 +0200

    plus_gitignore

--- backend/auth-service/models/user.model.js
+++ backend/auth-service/models/user.model.js
@@ -1,12 +1,3 @@
-// const mongoose = require("mongoose");
-
-// const userSchema = new mongoose.Schema({
-//   email: { type: String, required: true, unique: true },
-//   password: { type: String, required: true },
-//   role: { type: String, enum: ["client", "admin"], default: "client" },
-// });
-
-// module.exports = mongoose.model("User", userSchema);
 const { DataTypes } = require("sequelize");
 const { sequelize } = require("../config/db");
 
