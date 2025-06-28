2a331fcceef7d665215b070871ea28cdf6bec697
MokraneMassi <Mokranemassi06@gmail.com>
2025-06-28 16:48:05 +0200

    added_getuserbyID_DELETuserbyID

--- backend/auth-service/routes/auth.routes.js
+++ backend/auth-service/routes/auth.routes.js
@@ -10,12 +10,16 @@ const {
   register,
   login,
   getUsers,
+  getUserById,
   updateUser,
+  deleteUser,
 } = require("../controllers/auth.controller");
 
 router.post("/register", register);
 router.post("/login", login);
 router.get("/users", getUsers);
+router.get("/users/:id", getUserById);
 router.put("/users/:id", updateUser);
+router.delete("/users/:id", deleteUser);
 
 module.exports = router;
